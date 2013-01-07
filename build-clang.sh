b065d98ff41aaf72d3de80fdcb6efed16b69dddf
Jim Ursetto <jim@3e8.org>
2013-01-07 02:51:16 -0600

    Remove build-*.sh

--- build-clang.sh
+++
@@ -1,1 +0,0 @@
-make PLATFORM=macosx NURSERY=1048576 C_COMPILER=clang CHICKEN=$HOME/local/chicken-4.7.0-st-clang/bin/chicken PREFIX=$HOME/local/chicken-4.7.0-st-clang "$@"
--- build-llvm.sh
+++
@@ -1,2 +0,0 @@
-#make PLATFORM=macosx C_COMPILER=gcc-4.2 NURSERY=1048576 CHICKEN=$HOME/local/chicken-4.7.0/bin/chicken PREFIX=$HOME/local/chicken-4.7.0-st "$@"
-make PLATFORM=macosx NURSERY=1048576 CHICKEN=$HOME/local/chicken-4.7.0-st-llvm/bin/chicken PREFIX=$HOME/local/chicken-4.7.0-st-llvm "$@"
